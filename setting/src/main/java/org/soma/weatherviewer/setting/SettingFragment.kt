cf8e4019b61dca286587ffc4f00d16021ab5107e
ulalala <reyoula@gmail.com>
2022-09-06 00:54:49 +0900

    setting view

--- setting/src/main/java/org/soma/weatherviewer/setting/SettingFragment.kt
+++ setting/src/main/java/org/soma/weatherviewer/setting/SettingFragment.kt
@@ -5,13 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.soma.weatherviewer.common.MainActivityUtil
+import org.soma.weatherviewer.setting.databinding.FragmentSettingBinding
 
-class SettingFragment : Fragment() {
+class SettingFragment : Fragment(), SettingFragmentListener {
+
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        _binding = FragmentSettingBinding.inflate(inflater, container, false).also {
+            it.lifecycleOwner = this
+            it.listener = this
+        }
+        return binding.root
     }
+
+    override fun onClickBackButton() {
+        activity?.onBackPressedDispatcher?.onBackPressed()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
+
+interface SettingFragmentListener {
+    fun onClickBackButton()
 }
