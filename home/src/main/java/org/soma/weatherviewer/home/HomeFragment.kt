fbcdadfd0fc79ca5b17d32b6f63954b92385c99c
Hanbit-Kang <fchopinof99@gmail.com>
2022-09-29 15:35:38 +0900

    Implement HomeFragment to be switched by homeScreenOption in DataStore

--- home/src/main/java/org/soma/weatherviewer/home/HomeFragment.kt
+++ home/src/main/java/org/soma/weatherviewer/home/HomeFragment.kt
@@ -5,13 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import org.soma.weatherviewer.common.MainActivityUtil
+import org.soma.weatherviewer.common.domain.usecase.DataStoreUseCase
+import org.soma.weatherviewer.common.util.HomeScreenOptionType
 import org.soma.weatherviewer.home.databinding.FragmentHomeBinding
 import org.soma.weatherviewer.home.detail.WeatherDetailInfoFragment
+import org.soma.weatherviewer.home.list.WeatherListInfoFragment
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(), HomeFragmentListener {
+
+    @Inject lateinit var dataStoreUseCase: DataStoreUseCase
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -19,17 +32,29 @@ class HomeFragment : Fragment(), HomeFragmentListener {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false).apply {
             lifecycleOwner = this@HomeFragment
             listener = this@HomeFragment
         }
 
-        childFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, WeatherDetailInfoFragment())
-            .commit()
+        subscribeUi()
 
         return binding.root
+    }
+
+    private fun subscribeUi() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            dataStoreUseCase.homeScreenOption.collectLatest {
+                val fragment = when (it) {
+                    HomeScreenOptionType.Current.name -> WeatherDetailInfoFragment()
+                    else -> WeatherListInfoFragment()
+                }
+                childFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit()
+            }
+        }
     }
 
     override fun onClick5DaysButton() {
@@ -43,10 +68,6 @@ class HomeFragment : Fragment(), HomeFragmentListener {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    companion object {
-
     }
 }
 
