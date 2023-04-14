18a48853bd4ef1837ded5156f2407ca7863f88d2
JGeun <pjk8926@gmail.com>
2023-04-14 19:30:53 +0900

    Feat: Hilt 설정

---
+++ app/src/main/java/org/soma/weatherviewer/android/SomaWeatherViewerApplication.kt
@@ -0,0 +1,7 @@
+package org.soma.weatherviewer.android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SomaWeatherViewerApplication : Application()
