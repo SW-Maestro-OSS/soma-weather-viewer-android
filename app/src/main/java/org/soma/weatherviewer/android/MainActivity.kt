e0755241856f80953bc81da736b26c29a464635e
JGeun <pjk8926@gmail.com>
2023-04-15 00:25:28 +0900

    Feat: Navigation 연결 (#35)

--- app/src/main/java/org/soma/weatherviewer/android/MainActivity.kt
+++ app/src/main/java/org/soma/weatherviewer/android/MainActivity.kt
@@ -1,13 +1,26 @@
 package org.soma.weatherviewer.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import org.soma.weatherviewer.android.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+	private lateinit var binding: ActivityMainBinding
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+		supportActionBar?.hide()
+
+		val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+		val navController = navHostFragment.navController
+
+		binding.bottomNav.setupWithNavController(navController)
 	}
 }
---
+++ feature/forecast/src/main/java/org/soma/weatherviewer/forecast/ForecastFragment.kt
@@ -0,0 +1,25 @@
+package org.soma.weatherviewer.forecast
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ForecastFragment : Fragment() {
+
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_forecast, container, false)
+	}
+
+	companion object {
+
+		@JvmStatic
+		fun newInstance() = ForecastFragment()
+
+	}
+}
